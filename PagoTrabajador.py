2b0a7799294b8b6863831496ffc528ed65d3a943
carlospanohernandez <carlospano8@gmail.com>
2017-09-05 23:29:44 -0500

    Primera parte. Crear archivos.

--- PagoTrabajador.py
+++ PagoTrabajador.py
@@ -2,3 +2,33 @@
 #Autor: Carlos Pano Hernández
 
 #Descripción: Este programa calcula el pago total del trabajador de acuerdo a las horas extra trabajadas, incluyendo el pago de hora actual y horas normales trabajadas.
+
+def calcularPagoNormal(horasNormales,pagoHora):
+    pagoNormal=horasNormales*pagoHora
+
+    return pagoNormal
+
+def calcularPagoExtra(horasExtra,pagoHora):
+    pagoExtra=(horasExtra*pagoHora)+((horasExtra*pagoHora)/2)
+    return pagoExtra
+
+def calcularPagoTotal(normal,extra):
+    total=normal+extra
+    return total
+
+def main():
+    horasNormales=int(input("Teclea las horas normales trabajadas:"))
+    horasExtra=int(input("Teclea las horas extras trabajadas:"))
+    pagoHora=float(input("Teclea el pago por hora:"))
+
+    pagoN=calcularPagoNormal(horasNormales,pagoHora)
+    pagoE=calcularPagoExtra(horasExtra,pagoHora)
+    pagoT=calcularPagoTotal(pagoN,pagoE)
+
+    print("")
+    print("Pago normal: $%.2f"%(pagoN))
+    print("Pago extra: $%.2f"%(pagoE))
+    print("-----------------------------")
+    print("Pago total: $%.2f"%(pagoT))
+
+main()
