d1a773a36fd7cd62a8b4e941bc4f0a8f71b91fbb
Aaron Tonatiuh Villanueva Guzmán <30881287+AaronVillanueva@users.noreply.github.com>
2017-09-06 09:36:37 -0500

    Update Cálculo del pago de un trabajador.py
    Comentarios

--- Cálculo del pago de un trabajador.py
+++ Cálculo del pago de un trabajador.py
@@ -1,15 +1,18 @@
 #encoding: UTF-8
 #Autor: Aaron Tonatiuh Villanueva Guzmán
-#Este programa calculo el pago de un trabajador a partir de sus horas normales trabajadas así como las extra las cuales pagan 50% más
+#Este programa calcula el pago de un trabajador a partir de sus horas normales trabajadas así como las extra las cuales pagan 50% más
 
+#Esta función permite calcular el pago a partir de las horas trabajadas y el pago por hora.
 def calculopagoNormal(horasN, pagoH):
     pagoN=horasN*pagoH
     return(pagoN)
 
+#Esta función calcula el pago extra del trabajador suponiendo que cada hora extra es pagada con un 50% adicional.
 def calculopagoExtra(horasE, pagoH):
     pagoE=horasE* (pagoH*1.5)
     return(pagoE)
 
+#Esta función Main lee las horas normales, horas extra y el pago por hora para calcular el pago total por horas normales, horas extra y el pago total por ambas.
 def Main():
     horasNormales=int(input("Teclea las horas normales trabajadas: "))
     horasExtra=int(input("Teclea las horas extra trabajadas: "))
