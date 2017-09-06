acb90cd9cf5068e5ca786d15c8090a9f9a5fc715
Aaron Tonatiuh Villanueva Guzmán <30881287+AaronVillanueva@users.noreply.github.com>
2017-09-06 09:25:49 -0500

    Update Datos de una esfera.py

--- Datos de una esfera.py
+++ Datos de una esfera.py
@@ -3,16 +3,20 @@
 #Este programa recibe el radio de una esfera y calcula su volumen, diámetro y área.
 import math
 
+#Esta función realiza todos los calculos necesarios para saber la información requerida de la esfera. Regresa los valores con un tuple que después se desempaquetará.
 def calculos(radio):
-    areaEsfera=4 * math.pi * (radio**2)
-    volumenEsfera= ( 4 * math.pi * (radio**3)) / 3
-    diametroEsfera= radio * 2
-    return(areaEsfera, volumenEsfera, diametroEsfera)
+    arEsfera=4 * math.pi * (radio**2)
+    volEsfera= ( 4 * math.pi * (radio**3)) / 3
+    diaEsfera= radio * 2
+    return(arEsfera, volEsfera, diaEsfera)
 
+#Esta función Main lee el radio de la esfera para poder realizar los calculos pertinentes con la función calculos. Además, desempaqueta el tuple de calculos e imprime los resultados con formato.
 def Main():
     radio= float(input("Escribe el radio de la esfera: "))
-    calculosfinales=calculos(radio)
+    areaEsfera, volumenEsfera, diametroEsfera=calculos(radio)
     print("Esfera con radio", radio)
-    print(calculosfinales)
+    print("Diametro: %.2f"% diametroEsfera)
+    print("Volumen: %.2f"% volumenEsfera)
+    print("Area: %.2f"% areaEsfera)
 
 Main()
