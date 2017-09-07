075e8fe6b259517b3b99a56e18f07f25a83d2fb6
Gerardo Valderrama <A01374994@itesm.mx>
2017-09-07 10:42:22 -0500

    Ejercicio 4 Completado

--- rendimiento_autos.py
+++ rendimiento_autos.py
@@ -5,26 +5,32 @@
 #Programa que calcula cuanto kilometros por litro y millas por galon rinde de acuerdo a lo indicado por el usario, aproximar
 # cuantos litro se necsita por una cantidad de kilometos dada por el usuario.
 
+#funcion que calgula el rendimiento del aunto en kilometros por litro
 def calcularrendimientoKmL(km, l):
     rendimientoKilometroLitro = km / l
     return rendimientoKilometroLitro
 
+#Funcion que convierte los kilometros a Millas
 def convertirKmaMi(km):
     kilometroconvertido = km / 1.609344
     return kilometroconvertido
 
+#Función que convierte los litros a Galones
 def convertirLaGal(l):
     litrosconvertidos = l * .264172051
     return litrosconvertidos
 
+#Función que calcula el rendimiento del auto en Millas por Galon
 def calcularrendimientoMiGal(Mi,Gal):
     rendimientoMillaGalon = Mi / Gal
     return rendimientoMillaGalon
 
+#Función que calcula los litros esperados de caurdo a determinada cantidad de kilometros  que se recorreran
 def calcularaproximadoL(km1,l,kmesp):
     prediccionlitros = (kmesp*l)/km1
     return prediccionlitros
 
+#Función principal main que lee inputs, llama a las demas funciones e imprime
 def main():
     kilometros = float(input("Teclea los kilómetros recorridos: "))
     litros = float(input("Teclea los litros utilizados: "))
