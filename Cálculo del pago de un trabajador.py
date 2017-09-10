cc36b69d89f58f92148f402123c2cf698e80c200
A01374974@itesm.mx <Conq55m!>
2017-09-09 23:24:58 -0500

    tarea 03

--- Cálculo del pago de un trabajador.py
+++ Cálculo del pago de un trabajador.py
@@ -2,23 +2,23 @@
 #Alejandro Chávez Campos, A01374974
 #Este es para calcular el pago semanal de un trabajador a partir de sus horas normales, horas extras y el pago por hora.
 
-def calcularPago(horasNormales, pago):
+def calcularPago(horasNormales, pago): #Calcula el pago de las horas normales
     pagoNormal= horasNormales*pago
     return pagoNormal
 
 
-def calcularPagoExtra(horasExtras, pago):
+def calcularPagoExtra(horasExtras, pago): #Calcula el pago de las horas extras
     pagoExtra=horasExtras*(pago*1.5)
     return pagoExtra
 
 
-def main():
+def main(): #Programa principal
     horasNormales=float(input("Teclea las horas normales trabajadas: "))
     horasExtras= float(input ("Teclea las horas extras trabajadas: "))
     pago=float(input("Teclea el pago por hora: "))
     pagoNormal= calcularPago(horasNormales,pago)
     pagoExtra= calcularPagoExtra(horasExtras, pago)
-    pagoTotal= pagoNormal+pagoExtra
+    pagoTotal= pagoNormal+pagoExtra #Calcula el pago total sumando el pago normal y el extra
     print ()
     print("Pago normal: $%.2f"%pagoNormal)
     print("Pago extra: $%.2f"%pagoExtra)
